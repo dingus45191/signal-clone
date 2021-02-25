fca7d1e279943ddc9bd8353a32eded3008891be7
Mubashir Hasan <mubashirhasan716@gmail.com>
2021-02-25 15:32:45 +0530

    one error

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { View, Text } from "react-native";
 import { Avatar, ListItem } from "react-native-elements";
+import { auth, db } from "../firebase";
 
-export default function CustomListItem({id, chatName,enterChat}) {
+export default function CustomListItem({ id, chatName, enterChat, route }) {
+  const [chatMessages, setChatMessages] = useState([]);
+  useLayoutEffect(() => {
+    const unsubscribe = db
+      .collection("chats")
+      .doc(id)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+      .onSnapshot((snapshot) =>
+        setChatMessages(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+    return unsubscribe;
+  }, [route]);
   return (
-    <ListItem onPress={()=>{
-      enterChat(id, chatName)
-    }
-    } key={id} bottomDivider>
+    <ListItem
+      onPress={() => {
+        enterChat(id, chatName);
+      }}
+      key={id}
+      bottomDivider
+    >
       <Avatar
         rounded
         source={{
           uri:
-            "https://avatars.githubusercontent.com/u/60180419?s=460&u=5135c6af08fc8ae159e854882cd816efa82c7da5&v=4",
+            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.mwnI-qZ3xHBoFPwkIk9YVQHaE7%26pid%3DApi&f=1",
         }}
       />
       <ListItem.Content>
-        <ListItem.Title style={{ fontWeight: '800' }}>
+        <ListItem.Title style={{ fontWeight: "800" }}>
           {chatName}
         </ListItem.Title>
         <ListItem.Subtitle numberOfLines={1} ellipsizeMode={"tail"}>
-          Awesome
+          {chatMessages?.[0]?.displayName}:{chatMessages?.[0]?.message}
         </ListItem.Subtitle>
       </ListItem.Content>
     </ListItem>
